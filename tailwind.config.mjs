d44713b7774594ff3892786e307d764f49da2c2f
Chimmy <jimmyeetran@gmail.com>
2024-03-03 10:13:31 +1100

    added logo, font, and styling to blog

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,18 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'purple-light-old': '#F4EEFF',
+				'purple-dark-old': '#424874',
+				'purple-lavender-old': '#DCD6F7',
+				'purple-blue-old': '#A6B1E1',
+				'purple-heavy': '#352F44',
+				'purple-dark': '#5C5470',
+				'purple-light': '#B9B4C7',
+			},
+		},
+
 	},
 	plugins: [
 		require('@tailwindcss/typography'),
